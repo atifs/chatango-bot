89062b1eae5a6468c356e35051144e647e619f37
José <33236893+TheClonerx@users.noreply.github.com>
2017-10-30 19:28:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from libs import bot, config
+
+def main():
+    config.load_all()
+    my_bot = bot.Bot(config.auth["name"], config.auth["password"], True)
+    for room in config.rooms:
+        my_bot.joinRoom(room)
+    try:
+        my_bot.main()
+    finally:
+        config.save_all()
+
+
+if __name__ == '__main__':
+    main()

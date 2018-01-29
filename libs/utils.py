9fb23303701dc042c854e280164d3607840f109b
José <33236893+TheClonerx@users.noreply.github.com>
2018-01-29 04:49:33 -0300

    Update utils.py

--- libs/utils.py
+++ libs/utils.py
@@ -1,5 +1,6 @@
 import sys
-import sys
+import time
+from . import ch
 
 def arguments(*args, **kwargs):
     args = ", ".join(repr(x) for x in args)
@@ -10,8 +11,19 @@ def arguments(*args, **kwargs):
 
 def event(func):
     def wraper(*args, **kwargs):
-        print(f"{func.__name__}{arguments(*args, **kwargs)}", end=" -> ", file=sys.stderr)
-        r = func(*args, *kwargs)
-        print(r, file=sys.stderr)
-        return r
+        # args[0] is self, the bot
+        to_print = time.strftime("%X ")
+        for arg in args[1:]:
+            if isinstance(arg, ch.PM):
+                to_print += "[PM] "
+            elif isinstance(arg, ch.Room):
+                to_print += "[{}] ".format(arg.name)
+            elif isinstance(arg, ch._User):
+                to_print += "<{}> ".format(arg.name)
+            elif isinstance(arg, ch.Message):
+                to_print += repr(arg.body)[1:-1] + " "
+            else:
+                to_print += repr(arg)
+            print(to_print)
+            return func(*args, **kwargs)
     return wraper

acec660da5a91a45ffcf81f19811ff4a8c50fb0b
José <33236893+TheClonerx@users.noreply.github.com>
2018-01-29 04:56:29 -0300

    Update cmds.py

--- cmds/cmds.py
+++ cmds/cmds.py
@@ -1,1 +1,3 @@
-room.message(", ".join(__import__("os").path.splitext(x)[0] for x in __import__("os").listdir("cmds")))
+import os
+
+room.message(", ".join(os.path.splitext(x)[0] for x in os.listdir("cmds")))

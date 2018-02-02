71c8b3372c31779b6d1a7949e7ca8f4d5d67692e
José <33236893+TheClonerx@users.noreply.github.com>
2018-02-02 17:06:55 -0300

    Update config.py

--- libs/config.py
+++ libs/config.py
@@ -39,7 +39,7 @@ def load_langs():
                 data = line.strip().split(" ", 1)
                 if len(data) == 1 and data[0]:
                     raise ValueError("Invalid format")
-                if data[0]
+                if data[0]:
                     id, text = data
                     langs[lang][id] = text
 
